77514ab0137753662ff376ad3ab986e70e624d9a
rileyge <grlsr@163.com>
2021-04-09 22:49:18 +0800

    add util class to help use to sign and verify deploy.

---
+++ dotrchain/DeployData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotrchain
+{
+    public class DeployData
+    {
+        public string Term;
+        public long PhloPrice;
+        public long PhloLimit;
+        public long ValidAfterBlockNo = -1;
+        public long TimestampMillis = -1;
+        public DeployData(string term, long price = 1, long limit = 1000, long validAfterBlockNo = -1, long timestampMillis = -1)
+        {
+            Term = term;
+            PhloPrice = price;
+            PhloLimit = limit;
+            ValidAfterBlockNo = validAfterBlockNo;
+            TimestampMillis = timestampMillis;
+        }
+
+        public void SetTimestamp(DateTime dt)
+        {
+            var startTime = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+            TimestampMillis = (long)(dt - startTime).TotalMilliseconds;
+        }
+    }
+}
---
+++ dotrchain/Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Casper;
+
+namespace dotrchain
+{
+    public class Util
+    {
+        public static DeployDataProto SignDeploy(PrivateKey key, string term, long phloPrice = 1, long phloLimit = 1000, 
+            long validAfterBlockNo = -1, long timestampMillis = -1)
+        {
+            if (timestampMillis < 0) timestampMillis = DateTimeToUtc(DateTime.Now);
+
+            var data = new DeployDataProto
+            {
+                Deployer = key.PublicKey.Bytes,
+                Term = term,
+                phloPrice = phloPrice,
+                phloLimit = phloLimit,
+                validAfterBlockNumber = validAfterBlockNo,
+                Timestamp = timestampMillis,
+                sigAlgorithm = "secp256k1",                
+            };
+            data.Sig = key.Sign(GenDeployDataForSig(data));
+            return data;
+        }
+        private static byte[] GenDeployDataForSig(DeployDataProto deployData)
+        {
+            var data = new DeployDataProto()
+            {
+                Term = deployData.Term,
+                Timestamp = deployData.Timestamp,
+                phloLimit = deployData.phloLimit,
+                phloPrice = deployData.phloPrice,
+                validAfterBlockNumber = deployData.validAfterBlockNumber,
+            };
+            MemoryStream ms = new MemoryStream();
+            ProtoBuf.Serializer.Serialize(ms, data);            
+            return ms.ToArray();
+        }
+        public static bool VerifyDeploy(PublicKey key, byte[] sig, DeployDataProto data)
+        {
+            return key.Verify(sig, GenDeployDataForSig(data));
+        }
+
+        public static long DateTimeToUtc(DateTime dt)
+        {
+            var startTime = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+            return (long)(dt - startTime).TotalMilliseconds;
+        }
+    }
+}
